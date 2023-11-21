6014a1faaf472a30e14060fa3f50b700a1d3cd14
Hrushikesh Shelar <37904080+hrushikesh-shelar@users.noreply.github.com>
2023-11-21 13:07:48 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (86.79%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,6 +1,9 @@
-vector<int> dp(50, -1);
+
 class Solution {
+    vector<int> dp;
 public:
+    Solution() : dp(50, -1) {}
+
     int climbStairs(int n) {
         if(dp[n]!=-1) return dp[n];
         if(n==0 or n==1) {
