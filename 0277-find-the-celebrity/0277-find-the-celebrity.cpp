6e8cc93b8eebddad3898ee8885046fdb73877ecc
Hrushikesh Shelar <37904080+hrushikesh-shelar@users.noreply.github.com>
2023-12-10 22:55:53 +0530

    Time: 163 ms (39.21%), Space: 10.2 MB (23.65%) - LeetHub

---
+++ 0277-find-the-celebrity/0277-find-the-celebrity.cpp
@@ -0,0 +1,20 @@
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        int celeb = 0;
+        for(int i=1; i<n; i++) {
+            if(!knows(i, celeb) and knows(celeb, i)) {
+                celeb = i;
+            }
+        }
+        for(int i=0; i<n; i++) {
+            if(celeb!=i and (!knows(i, celeb) or knows(celeb, i))) {
+                return -1;
+            }
+        }
+        return celeb;
+    }
+};
