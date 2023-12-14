1c9d0c6abc968f9efda048ff11d96979173a49c3
Hrushikesh Shelar <37904080+hrushikesh-shelar@users.noreply.github.com>
2023-12-14 14:23:45 +0530

    Time: 2 ms (54.02%), Space: 9.3 MB (6.48%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.rbegin(), citations.rend());
+        priority_queue<int> maxHeap;
         int n = citations.size();
-        int i = 0;
-        while (i < n && citations[i] > i) {
-            i++;
+        for(int i=0; i<n; i++) {
+            maxHeap.push(citations[i]);
         }
+        int i =0;
+        while (maxHeap.size()>0 && maxHeap.top() > i) {
+            i++;
+            // cout<<"i:"<<i<<endl;
+            maxHeap.pop();
+        }
+        // cout<<"Final i:"<<i<<endl;
         return i;
     }
 };
