3a56072aff56921b221d474d40f19fde7bf7a4a3
Hrushikesh Shelar <37904080+hrushikesh-shelar@users.noreply.github.com>
2024-01-04 00:08:05 +0530

    Time: 106 ms (21.88%), Space: 23.3 MB (96.49%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -5,23 +5,15 @@ public:
         int prevDevices = 0;
         int currDevices = 0;
         for(int row=0; row<bank.size(); row++) {
-            if(prevDevices==0) {
-                for(int col=0; col<bank[row].size(); col++) {
-                    if(bank[row][col]=='1') {
-                        prevDevices++;
-                    }
+            currDevices=0;
+            for(int col=0; col<bank[row].size(); col++) {
+                if(bank[row][col]=='1') {
+                    currDevices++;
                 }
-            } else {
-                currDevices=0;
-                for(int col=0; col<bank[row].size(); col++) {
-                    if(bank[row][col]=='1') {
-                        currDevices++;
-                    }
-                }
-                if(currDevices!=0) {
-                    res += prevDevices*currDevices;
-                    prevDevices = currDevices;   
-                }
+            }
+            if(currDevices!=0) {
+                res += prevDevices*currDevices;
+                prevDevices = currDevices;   
             }
         }
         return res;
