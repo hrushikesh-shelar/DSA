f1350d6775b72c51ba9f1f5dd73457f7cf690575
Hrushikesh Shelar <37904080+hrushikesh-shelar@users.noreply.github.com>
2024-01-19 00:55:18 +0530

    Time: 1 ms (45.29%), Space: 6.3 MB (71.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,16 @@
-
 class Solution {
     vector<int> dp;
 public:
-    Solution() : dp(50, -1) {}
-
+    Solution() {
+        dp.resize(50, 0);
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+    }
     int climbStairs(int n) {
-        if(dp[n]!=-1) return dp[n];
-        if(n==0 or n==1) {
-            return dp[n]=1;
+        if(dp[n]!=0) {
+            return dp[n];
         }
-        return dp[n] = ( climbStairs(n-1) + climbStairs(n-2) );
+        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
     }
 };
